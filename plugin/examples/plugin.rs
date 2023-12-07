1f559708f6bacb4c47e30e61d833b222157751c0
zach <zach@dylibso.com>
2023-12-06 16:53:27 -0800

    wip: simplify plugin

--- plugin/examples/plugin.rs
+++ plugin/examples/plugin.rs
@@ -5,25 +5,21 @@ use helix_plugin::*;
 
 #[plugin_fn]
 pub fn vsplit_sel() -> FnResult<()> {
-    let initial = Editor.view();
     for sel in Editor.selections() {
         Editor.execute(":vsplit-new")?;
         Editor.add_selection(0, 0);
         let txt = sel.text()?;
         Editor.insert_text(&txt, Insert::BeforeSelection)?;
-        Editor.focus(initial);
     }
     Ok(())
 }
 
 #[plugin_fn]
 pub fn open_sel() -> FnResult<()> {
-    let initial = Editor.view();
     for sel in Editor.selections() {
         let txt = sel.text()?;
         Editor.execute(":vsplit-new")?;
         Editor.open(&txt)?;
-        Editor.focus(initial);
     }
     Ok(())
 }
