08acd6e5a54e8bf7dc88c5c8de150284edb40496
zach <zach@dylibso.com>
2023-12-19 14:24:44 -0800

    wip: simplify a.rs code, add example config

--- plugin/examples/a.rs
+++ plugin/examples/a.rs
@@ -6,14 +6,15 @@ use helix_plugin::*;
 const EXTENSIONS: &'static [(&'static str, &'static str)] =
     &[("c", "h"), ("cpp", "hpp"), ("cc", "hh"), ("ml", "mli")];
 
-#[plugin_fn]
-pub fn switch_header() -> FnResult<()> {
+fn switch_header_impl<F: Fn() -> Result<(), Error>>(f: F) -> FnResult<()> {
     if let Some(path) = Editor.path()? {
         if let Some(ext) = path.extension() {
             for (k, v) in EXTENSIONS {
                 if &ext == k {
+                    f()?;
                     Editor.open(path.with_extension(v))?;
                 } else if &ext == v {
+                    f()?;
                     Editor.open(path.with_extension(k))?;
                 }
             }
@@ -23,48 +24,21 @@ pub fn switch_header() -> FnResult<()> {
     }
 
     Ok(())
+}
+
+#[plugin_fn]
+pub fn switch_header() -> FnResult<()> {
+    switch_header_impl(|| Ok(()))
 }
 
 #[plugin_fn]
 pub fn vsplit_header() -> FnResult<()> {
-    if let Some(path) = Editor.path()? {
-        if let Some(ext) = path.extension() {
-            for (k, v) in EXTENSIONS {
-                if &ext == k {
-                    Editor.execute(":vsplit-new")?;
-                    Editor.open(path.with_extension(v))?;
-                } else if &ext == v {
-                    Editor.execute(":vsplit-new")?;
-                    Editor.open(path.with_extension(k))?;
-                }
-            }
-        }
-    } else {
-        Editor.set_status("Unable to determine file name")?;
-    }
-
-    Ok(())
+    switch_header_impl(|| Editor.execute(":vsplit-new"))
 }
 
 #[plugin_fn]
 pub fn hsplit_header() -> FnResult<()> {
-    if let Some(path) = Editor.path()? {
-        if let Some(ext) = path.extension() {
-            for (k, v) in EXTENSIONS {
-                if &ext == k {
-                    Editor.execute(":hsplit-new")?;
-                    Editor.open(path.with_extension(v))?;
-                } else if &ext == v {
-                    Editor.execute(":hsplit-new")?;
-                    Editor.open(path.with_extension(k))?;
-                }
-            }
-        }
-    } else {
-        Editor.set_status("Unable to determine file name")?;
-    }
-
-    Ok(())
+    switch_header_impl(|| Editor.execute(":hsplit-new"))
 }
 
 #[plugin_fn]
